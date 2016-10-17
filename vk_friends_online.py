d0fe3a1cae005d2b4db30a1bbb56de8744395ac6
Jack <mtjeck@gmail.com>
2016-10-17 23:15:03 +0300

    improve code

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -10,7 +10,7 @@ def get_user_password():
     return getpass.getpass()
 
 
-def get_online_friends(login, password):
+def get_online_friends(login, password, APP_ID):
     session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
@@ -19,21 +19,17 @@ def get_online_friends(login, password):
     )
     api = vk.API(session)
     friend_online_ids = api.friends.getOnline()
-    friend_online_names = api.users.get(user_ids=friend_online_ids)
-    friend_names = []
-    for friend in friend_online_names:
-        friend_name = "%s %s" % (friend['first_name'], friend['last_name'])
-        friend_names.append(friend_name)
-    return friend_names
+    return api.users.get(user_ids=friend_online_ids)
 
 
 def output_friends_to_console(friends_online):
     for friend in friends_online:
-        print(friend)
+        print(friend['first_name'], friend['last_name'])
+
 
 if __name__ == '__main__':
     APP_ID = int(input('APP ID: '))  # get here: https://vk.com/dev
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
+    friends_online = get_online_friends(login, password, APP_ID)
     output_friends_to_console(friends_online)
